a38b683a66f42d26f44c9a56c969f29c34d7a96d
Dynast86 <sweetylove2k@gmail.com>
2022-07-21 21:45:30 +0900

    Update ApiService.kt

--- app/src/main/java/com/dynast/weather/data/ApiService.kt
+++ app/src/main/java/com/dynast/weather/data/ApiService.kt
@@ -9,7 +9,9 @@ interface ApiService {
         const val ApiEndPoint: String = "v1/forecast"
     }
 
-    @GET("$ApiEndPoint?current_weather=true&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode,windspeed_10m,winddirection_10m&timezone=Asia%2FSeoul")
+    @GET("$ApiEndPoint?current_weather=true" +
+            "&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode,windspeed_10m,winddirection_10m&timezone=Asia%2FSeoul" +
+            "&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,shortwave_radiation_sum,et0_fao_evapotranspiration")
     suspend fun getWeatherData(
         @Query("latitude") latitude: Double,
         @Query("longitude") occupation: Double,
